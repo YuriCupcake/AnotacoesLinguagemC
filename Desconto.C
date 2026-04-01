04377b854a6dd33257740fdcae21db8c9b93c8f5
YuriCupcake <va6639879@gmail.com>
2026-04-01 23:25:00 +0000

    EXERCICIO2

---
+++ Desconto.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float valor;
+    printf ("DIGITE UM VALOR:\n");
+   scanf("%f" , &valor);
+
+ if(valor >= 100) {
+     printf("RECEBE DESCONTO");
+ }
+ else  {
+     printf("SEM DESCONTO");
+     
+}
+    return 0;
+}

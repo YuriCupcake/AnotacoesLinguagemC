237eaa9b5f37a5166e62d31ec7e08f4d4b7b3bd8
YuriCupcake <va6639879@gmail.com>
2026-04-22 20:25:11 -0300

    Lista3/Exercicio1.c

---
+++ Lista3/Exercicio1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int biometria, senha, modoSeguro;
+
+    printf("Digite a biometria (1/0): ");
+    scanf("%d", &biometria);
+
+    printf("Digite a senha (1/0): ");
+    scanf("%d", &senha);
+
+    printf("Digite o modo seguro (1/0): ");
+    scanf("%d", &modoSeguro);
+
+   if(modoSeguro==1) {
+       if(senha==1) {
+           printf("desbloqueado");
+       } else {
+           printf("bloqueado");
+       }
+       
+   } else {
+       if(senha || biometria) {
+           printf("desbloqueado");
+       } else {
+           printf("bloqueado");
+       }
+   } 
+       return 0;
+}
